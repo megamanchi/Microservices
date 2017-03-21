ac6fb28a13b01b3afc3f80030715bf5408db4005
mega <zerodump2@gmail.com>
2017-03-21 12:09:24 -0300

    zz

--- src/main/java/io/pivotal/microservices/accounts/AccountsController.java
+++ src/main/java/io/pivotal/microservices/accounts/AccountsController.java
@@ -77,6 +77,7 @@ public class AccountsController {
 	    CuentaParser cuenta= parser.traslateOutput();
 		logger.info("merge");
 	    logger.info("merge2");
+		  logger.info("merge2");
 		return cuenta;
 	}
 
